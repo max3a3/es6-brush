8828bb9e5030b5fbed8b1a831ef3c61e68d8d2da
wilkin <wilkin4@gmail.com>
2019-10-07 13:12:37 -0700

    clean up, run on local

--- symmetry/HTransform.js
+++ symmetry/HTransform.js
@@ -1,16 +1,17 @@
 import {TransformMode} from '../components/basic_class'
-function getHorizontalTransform()
-{
-		var newMode = new TransformMode();
-		
-		moveId = "move_" + mode;
-		
-		if(mode == "symmetry_H")
-		{
-			newMode.setMirror(new Point(0, vCenter), new Point(_canvasWidth, vCenter));
-			
-			moveX = '16px';
-			moveY = vCenter - 8 + _appBarHeight + 'px';
-		}
-  return newMode
+
+let moveId
+let moveX, moveY // the center?
+function getHorizontalTransform() {
+    var newMode = new TransformMode();
+
+    moveId = "move_" + mode;
+    const mode ='symmetry_H'
+    if (mode === "symmetry_H") {
+        newMode.setMirror(new Point(0, vCenter), new Point(_canvasWidth, vCenter));
+
+        moveX = '16px';
+        moveY = vCenter - 8 + _appBarHeight + 'px';
+    }
+    return newMode
 }
