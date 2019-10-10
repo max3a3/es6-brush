a9ea8b120c732b9d900d0d544830d71c42dc23fc
max3a3 <isplitpoly@gmail.com>
2019-10-10 11:18:22 -0700

    move event handler to div

--- components/App.js
+++ components/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import BrushCanvas from "./BrushCanvas";
-import Canvas from "./Canvas";
+import WidgetCanvas from "./WidgetCanvas";
 
 export default function App() {
   return (
-    <div>
+      {/*<WidgetCanvas /> put it inside BrushCanvas*/}
       <BrushCanvas />
-    </div>
   );
 }
--- components/BrushCanvas.js
+++ components/BrushCanvas.js
@@ -1,35 +1,54 @@
-import {Component} from "react";
+import { Component, Fragment } from "react";
 import React from "react";
 import invariant from "invariant";
 
-import getBrush, {initBrush} from "./brush_class";
-import {_canvasWidth, _canvasHeight} from "../tconfig";
+import getBrush, { initBrush } from "./brush_class";
+import { _canvasWidth, _canvasHeight } from "../tconfig";
 
 export default class BrushCanvas extends Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
-    this.brushColor = 'blue'
-      this.brushSize = 2
-        this.transformModes = []
-      this.shadow = false
+    this.canvasOverlayRef = React.createRef();
+    this.brushColor = "blue";
+    this.brushSize = 2;
+    this.transformModes = [];
+    this.shadow = false;
   }
+  drawOverlay(canvas, context) {
+    context.globalCompositeOperation = "source-over";
 
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.lineWidth = 3;
+    context.strokeStyle = "green";
+
+    context.beginPath();
+    context.moveTo(0, 50);
+    context.lineTo(100, 50);
+    context.stroke();
+  }
   componentDidMount() {
     invariant(this.canvasRef.current, "ref not inited");
     let context = this.canvasRef.current.getContext("2d");
     invariant(context, "no context");
     initBrush(context);
     this.brush = getBrush();
+
+    if (this.canvasOverlayRef.current) {
+      let overlay = this.canvasOverlayRef.current.getContext("2d");
+      this.drawOverlay(this.canvasOverlayRef.current, overlay);
+    }
   }
 
   mouseDown = event => {
+    console.log('mousedown')
     let [x, y] = this.getLocalXY(event);
     getBrush().beginStroke(
       this.brushColor,
       this.brushSize,
-      this.transformModes,
-      x, y,
+      this.transformModes, 
+      x,
+      y,
       this.shadow
     );
     this.isDrawing = true;
@@ -62,21 +81,30 @@ export default class BrushCanvas extends Component {
     if (this.isDrawing) {
       this.brush.endStroke();
     }
-    this.isDrawing = false;
+    this.isDrawing = false; 
   };
 
   render() {
     return (
-      <canvas
-        className="canvas"
-        ref={this.canvasRef}
-        height={_canvasHeight}
-        width={_canvasWidth}
-        id="canvas"
+      <div className='canvas_container
         onMouseDown={this.mouseDown}
         onMouseUp={this.mouseUp}
         onMouseMove={this.mouseMove}
-      />
+      >
+        <canvas
+          className="canvas"
+          ref={this.canvasRef}
+          height={_canvasHeight}
+          width={_canvasWidth}
+        />
+
+        <canvas
+          className="canvas_overlay"
+          ref={this.canvasOverlayRef}
+          height={100}
+          width={_canvasWidth}
+        />
+      </div>
     );
   }
 }
