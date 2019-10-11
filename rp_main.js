7be279228546c3ca4d52b901bdbd40787766c9e4
max3a3 <isplitpoly@gmail.com>
2019-10-10 21:47:47 -0700

    react paper demo wip

--- rp_main.js
+++ rp_main.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 
 //  @psychobolt/react-paperjs
-import { Rectangle, PaperContainer } from "@psychobolt/react-paperjs";
-function App() {
+import { Rectangle, PaperContainer,renderWithPaperScope } from "@psychobolt/react-paperjs";
+function Appx() {
   return (
     <PaperContainer
       className="flex_item"
@@ -18,5 +18,45 @@ function App() {
       />
     </PaperContainer>
   );
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    invariant(this.state.paper, "paper not defind");
+  }
+  render() {
+    let paperSetter = null;
+
+    // only called once, we install a component as that is the only way scope get passed
+    if (!this.state.paper) {
+      // register paper global to use for debugging
+      const RegisterPaperComp = () => {
+        return ,(paper => {
+          this.setState({ paper });
+          return null;
+        });
+      };
+      paperSetter = <RegisterPaperComp />;
+    }
+
+    return (
+      <PaperContainer
+        className="flex_item"
+        canvasProps={{ width: 400, height: 300, class: "tool_canvas" }}
+      >
+        {paperSetter}
+        <Rectangle
+          position={[90, 60]}
+          width={90}
+          height={60}
+          strokeColor="red"
+          fillColor="blue"
+        />
+      </PaperContainer>
+    );
+  }
 }
 render(<App />, document.getElementById("root"));
