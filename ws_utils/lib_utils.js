4224409dca0438562d721ea5801c9593f62932e1
wilkin <wilkin4@gmail.com>
2019-10-27 14:18:35 -0700

    initer work in browser

---
+++ ws_utils/lib_utils.js
@@ -0,0 +1,22 @@
+export let __bind = function (fn, me) {
+  return function () {
+    return fn.apply(me, arguments);
+  };
+}
+export let __hasProp = {}.hasOwnProperty
+export let __extends = function (child, parent) {
+  for (var key in parent) {
+    if (__hasProp.call(parent, key)) child[key] = parent[key];
+  }
+
+  function ctor() {
+    this.constructor = child;
+  }
+
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor();
+  child.__super__ = parent.prototype;
+  return child;
+}
+export let __slice = [].slice;
+
